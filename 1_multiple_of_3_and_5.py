9fc5a27203bd517ff56bbdaefb0716c0b3adee96
wesneubeck <wesneubeck@gmail.com>
2020-01-05 19:21:54 -0500

    change name of 1st commit

---
+++ 1_multiple_of_3_and_5.py
@@ -0,0 +1,26 @@
+# Project Euler: Problem 1: Multiples of 3 and 5
+
+
+#  If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+#  we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+
+def multiples(n):
+  multi = []
+  for i in range(1,n):
+    if i%3==0 or i%5==0:
+      multi.append(i)
+
+  return sum(multi)
+    
+
+
+
+print(multiples(8456))
+
+# multiplesOf3and5(1000) should return 233168.
+# multiplesOf3and5(49) should return 543.
+# multiplesOf3and5(19564) should return 89301183.
+# multiplesOf3and5(8456) should return 16687353.
+
